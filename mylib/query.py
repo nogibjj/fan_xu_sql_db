4394d2d635b39a25f7937d24cc18163cff3608b5
Fan Xu <fanxu2017@gmail.com>
2024-10-06 21:22:41 -0400

    query edits

--- mylib/query.py
+++ mylib/query.py
@@ -3,12 +3,51 @@
 import sqlite3
 
 
-def query():
-    """Query the database for the top 5 rows of the NBA_2015 table"""
+def create():
+    """Create a fake data"""
+    conn = sqlite3.connect("NBA_2015.db")
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO NBA_2015 VALUES "
+        "(Lebron James,SF,lebron-james,2003,100,100,100,100,100)"
+    )
+    conn.commit()
+    conn.close()
+    return "Sucessfully created!"
+
+
+def read():
+    """Read and print the database for all the rows of the table"""
     conn = sqlite3.connect("NBA_2015.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM NBA_2015")
-    print("Top 5 rows of the NBA_2015 table:")
     print(cursor.fetchall())
     conn.close()
-    return "Success"
+    return "Successfully read!"
+
+
+def update():
+    """Update bust status if bust percentage above 50%"""
+    conn = sqlite3.connect("NBA_2015.db")
+    cursor = conn.cursor()
+    cursor.execute("UPDATE NBA_2015 SET Bust = 'LOSER' WHERE Bust > '0.5';")
+    conn.commit()
+    conn.close()
+    return "Successfully updated!"
+
+
+def delete():
+    """Delete all rows where position is center"""
+    conn = sqlite3.connect("NBA_2015.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM NBA_2015 WHERE Position = 'C';")
+    conn.commit()
+    conn.close()
+    return "Sucessfully deleted!"
+
+
+if __name__ == "__main__":
+    create()
+    read()
+    update()
+    delete()
--- test_main.py
+++ test_main.py
@@ -5,22 +5,37 @@ Test goes here
 
 from mylib.extract import extract
 from mylib.transform_load import load
-from mylib.query import query
+from mylib.query import create, read, update, delete
 
 
 def test_extract():
     assert extract() == "data/historical_projections.csv"
 
 
-def test_query():
-    assert query() == "Success"
-
-
 def test_load():
     assert load() == "NBA_2015.db"
+
+
+def test_create():
+    assert create() == "Sucessfully created!"
+
+
+def test_read():
+    assert read() == "Successfully read!"
+
+
+def test_update():
+    assert update() == "Successfully updated!"
+
+
+def test_delete():
+    assert delete() == "Sucessfully deleted!"
 
 
 if __name__ == "__main__":
     test_extract()
     test_load()
-    test_query()
+    test_create()
+    test_read()
+    test_update()
+    test_delete()
